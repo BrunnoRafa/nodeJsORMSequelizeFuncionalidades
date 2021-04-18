4ad3735da5d9813edafafd87fc1e782d97905021
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2021-04-18 12:05:52 -0300

    adicionando scopo e validação no campo e-mail

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -3,9 +3,29 @@ module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
-  }, { paranoid: true });
+  }, {
+    paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true
+      }
+    },
+    scopes: {
+      todos: {
+        where: {}
+      }
+    }
+  });
   Pessoas.associate = function (models) {
     Pessoas.hasMany(models.Turmas, {
       foreignKey: 'docente_id'
